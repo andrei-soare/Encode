be626d43ee04f1e9267e8dcc47bc968940ea7e39
Andrei Soare <soare.andrei.florin@gmail.com>
2026-07-04 13:58:13 +0300

    problem solved

---
+++ Encode/EncodeStringTask.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Encode;
+
+public class EncodeStringTask
+{
+    public static string Encode(string input)
+    {
+        if(string.IsNullOrEmpty(input))
+        {
+            return "";
+        }
+
+        StringBuilder encodedString = new StringBuilder();
+
+        int count = 1;
+
+        for(int i = 1; i < input.Length; i++)
+        {
+            if (input[i] == input[i - 1])
+            {
+                count++;
+            }
+            else
+            {
+                encodedString.Append(input[i - 1]);
+                encodedString.Append(count);
+                count = 1;
+            }
+        }
+
+        encodedString.Append(input[input.Length - 1]);
+        encodedString.Append(count);
+
+        return encodedString.ToString();
+    }
+
+}
